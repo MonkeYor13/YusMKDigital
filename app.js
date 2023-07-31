84790290cd2da945e66ffb7f41dbc68cc8439056
MonkeYor13 <monkeyor13@gmail.com>
2023-07-31 12:18:07 -0300

    inicio de animacion

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+gsap.to('.menu-home',{
+  duration:5,
+  opacity: 1,
+  });
 
 
+  let tl = gsap.timeline({
+    repeat: Infinity,
+    yoyo: true,
+    delay:5,
+    });
+    
+  tl.to('.menu-titulo',{
+    duration:2,
+    x:10, 
+    });
+   
 
